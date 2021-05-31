add041fa6943027a7ae022bb8cc6f18143d85c5e
huginn <huginn@huginn.me>
2021-05-31 08:24:57 +0200

    implemented value class

--- Scripts/Core/Values/Value.cs
+++ Scripts/Core/Values/Value.cs
@@ -1,6 +1,35 @@
 namespace HoodedCrow.uCore.Core
 {
+    using System;
+
     public class Value<T>
     {
+        private Action<T> _onValueChange;
+        private T _value;
+
+        public T GetValue()
+        {
+            return _value;
+        }
+        public void SetValue(T value)
+        {
+            _value = value;
+        }
+
+        public void UpdateValue(T value)
+        {
+            SetValue(value);
+            _onValueChange.Invoke(_value);
+        }
+
+        public void AddListener(Action<T> callback)
+        {
+            _onValueChange += callback;
+        }
+
+        public void RemoveListener(Action<T> callback)
+        {
+            _onValueChange -= callback;
+        }
     }
 }
