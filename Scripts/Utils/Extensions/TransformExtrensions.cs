9b5a954756b738f1ec0adfb9c003532d6a640ec2
huginn <huginn@huginn.me>
2021-05-31 08:56:08 +0200

    implemented Transform.FindParentWithComponent extension method

--- Scripts/Utils/Extensions/TransformExtrensions.cs
+++ Scripts/Utils/Extensions/TransformExtrensions.cs
@@ -1,7 +1,29 @@
 namespace HoodedCrow.uCore.Utils
 {
+    using UnityEngine;
+
     public static class TransformExtrensions
     {
+        public static T FindParentWithComponent<T>(this Transform transform) where T : MonoBehaviour
+        {
+            T component = null;
+            Transform t = transform;
+            while (component == null)
+            {
+                t = transform.parent;
+                if (t == null)
+                {
+                    throw new NoParentsWithComponentException($"Cannot find parent with component: {typeof(T).Name} not found");
+                }
 
+                component = t.GetComponent<T>();
+                if (component != null)
+                {
+                    break;
+                }
+            }
+
+            return component;
+        }
     }
 }
