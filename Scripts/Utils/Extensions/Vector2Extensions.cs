74c9f66eebc7562fe4d0d2d63bd4a13f1be4b053
huginn <huginn@huginn.me>
2021-05-31 10:01:27 +0200

    implemented Vector2 extension methods

--- Scripts/Utils/Extensions/Vector2Extensions.cs
+++ Scripts/Utils/Extensions/Vector2Extensions.cs
@@ -1,7 +1,19 @@
 namespace HoodedCrow.uCore.Utils
 {
+    using UnityEngine;
+
     public static class Vector2Extensions
     {
+        public static Vector2 SetX(this Vector2 vector2, float value)
+        {
+            vector2.x = value;
+            return vector2;
+        }
 
+        public static Vector2 SetY(this Vector2 vector2, float value)
+        {
+            vector2.y = value;
+            return vector2;
+        }
     }
 }
