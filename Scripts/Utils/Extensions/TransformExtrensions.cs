67d3709cdd023ab8fb5f0cefdf29e9d02cf5250c
huginn <huginn@huginn.me>
2021-05-31 09:11:11 +0200

    implemented Transform.GetChildren extension method

--- Scripts/Utils/Extensions/TransformExtrensions.cs
+++ Scripts/Utils/Extensions/TransformExtrensions.cs
@@ -1,9 +1,21 @@
 namespace HoodedCrow.uCore.Utils
 {
+    using System.Collections.Generic;
     using UnityEngine;
 
     public static class TransformExtrensions
     {
+        public static List<Transform> GetChildren(this Transform transform)
+        {
+            List<Transform> children = new List<Transform>();
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                children.Add(transform.GetChild(i));
+            }
+
+            return children;
+        }
+
         public static T FindParentWithComponent<T>(this Transform transform) where T : MonoBehaviour
         {
             T component = null;
