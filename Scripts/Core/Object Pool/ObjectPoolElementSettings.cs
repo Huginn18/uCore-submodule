cb0a62f2974c8af8ede75d793d95dabd77999c2c
huginn <huginn@huginn.me>
2021-06-01 12:40:52 +0200

    implemented object pooling

---
+++ Scripts/Core/Object Pool/ObjectPoolElementSettings.cs
@@ -0,0 +1,20 @@
+namespace HoodedCrow.uCore.Core
+{
+    using System;
+    using UnityEngine;
+    using Object = System.Object;
+
+    [Serializable]
+    public class ObjectPoolElementSettings
+    {
+        public string Naming => _naming;
+        public GameObject Prefab => _prefab;
+        public Transform Parent => _parent;
+        public int MaxCapacity => _maxCapacity;
+
+        [SerializeField] private string _naming = string.Empty;
+        [SerializeField] private GameObject _prefab = null;
+        [SerializeField] private Transform _parent = null;
+        [SerializeField] private int _maxCapacity = 10;
+    }
+}
