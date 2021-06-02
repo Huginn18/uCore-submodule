a6220d062e03798e1567ac6ed35ca10c0cf42d3d
huginn <huginn@huginn.me>
2021-06-02 10:19:32 +0200

    implemented AMessage

---
+++ Scripts/Core/Messages/AMessage.cs
@@ -0,0 +1,36 @@
+namespace HoodedCrow.uCore.Core
+{
+    using System;
+    using UnityEngine;
+
+    public abstract class AMessage<TContent>: IMessage<TContent> where TContent: IMessageContent
+    {
+        private Action<TContent> _callback;
+
+        public void AddListener(Action<TContent> callback)
+        {
+            _callback += callback;
+        }
+
+        public void RemoveListener(Action<TContent> callback)
+        {
+            _callback -= callback;
+        }
+
+        public void Clear()
+        {
+            _callback = null;
+        }
+
+        public void Send(TContent content)
+        {
+            if (_callback == null)
+            {
+                Debug.LogWarning($"{GetType().Name} was sent but no one is listening to it.");
+                return;
+            }
+
+            _callback.Invoke(content);
+        }
+    }
+}
